ff821cbbb141501965428b95ec534d0a61481938
seb <>
2008-10-02 21:12:09 +0000

    started documenting the cal-i publisher functions

--- cal/info/cal-info.h
+++ cal/info/cal-info.h
@@ -1,4 +1,12 @@
 
+//!
+//! \file cal-info.h
+//!
+//! \brief the CAL Information interface
+//!
+//! \author Sebastian Kuzminsky <seb@highlab.com>
+//!
+
 #ifndef __CAL_INFO_H
 #define __CAL_INFO_H
 
@@ -9,20 +17,58 @@
 
 
 
+//! This struct provides the CAL-I interface
 typedef struct {
     //
     // publisher stuff
     //
 
-    // fills out this->addr and this->socket
+
+    //!
+    //! \brief Make this peer available for connections.
+    //!
+    //! \param this A handle for the CAL Peer that is going to start
+    //!     accepting connections.  This peer's addressing information will
+    //!     be set to indicate how other peers can connect to it.
+    //!
+    //! \param callback The callback function to be called by the CAL
+    //!     library when events happen to this peer.
+    //!
+    //! \return On success, returns a file descriptor which should be
+    //!     monitored by the caller.  When the fd is readable, the caller
+    //!     should call the CAL-I publisher_read() function.  The caller
+    //!     must never read or write the returned file descriptor.
+    //!     On failure, returns -1.
+    //!
+
     int (*init_publisher)(cal_peer_t *this, void (*callback)(cal_event_t *event));
+
+
+    //! \brief Just a place-holder for now
     void (*publish)(char *topic, void *msg, int size);
-    int (*publisher_read)(void);  // maybe calls callback
+
+
+    //!
+    //! \brief Service the CAL-I publisher's file descriptor.
+    //!
+    //! This function should be called whenever the file descriptor
+    //! returned from init_publisher() is readable.  It will service the
+    //! file descriptor and may call the callback with an appropriate
+    //! event.
+    //!
+    //! \return True (non-zero) on success.  False (zero) on failure,
+    //!     indicating that the file descriptor returned from
+    //!     init_publisher() is invalid and the caller needs to re-call
+    //!     init_publisher() to get a new fd.
+    //!
+
+    int (*publisher_read)(void);
 
 
     //
     // subscriber stuff
     //
+
 
     int (*init_subscriber)(void (*callback)(cal_event_t *event));
     void (*cancel_subscriber)(void);
