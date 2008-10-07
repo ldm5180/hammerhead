cc5e6fb3e217c0592cfeda12dfdeb00d34095dc3
seb <>
2008-10-07 21:05:44 +0000

    shutdown CAL properly on exit

--- cal/examples/time-subscriber.c
+++ cal/examples/time-subscriber.c
@@ -1,12 +1,14 @@
 
 #include <errno.h>
-#include <netinet/in.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 #include <arpa/inet.h>
+
+#include <netinet/in.h>
 
 #include <sys/socket.h>
 
@@ -77,8 +79,61 @@ void cal_callback(cal_event_t *event) {
 
 
 
+static void exit_signal_handler(int signal_number) {
+    exit(0);
+}
+
+
+static void exit_handler(void) {
+    cal_client.shutdown();
+}
+
+
+void make_shutdowns_clean(void) {
+    struct sigaction sa;
+
+    atexit(exit_handler);
+
+    // handle exit signals
+    sa.sa_handler = exit_signal_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+
+    if (sigaction(SIGINT, &sa, NULL) < 0) {
+        fprintf(stderr, "sigaction(SIGINT, ...): %s", strerror(errno));
+        exit(1);
+    }
+
+    if (sigaction(SIGTERM, &sa, NULL) < 0) {
+        fprintf(stderr, "sigaction(SIGTERM, ...): %s", strerror(errno));
+        exit(1);
+    }
+
+
+    //
+    // If a network connection is lost somehow, writes will cause the
+    // process to receive SIGPIPE, and the default SIGPIPE handler
+    // terminates the process.  Not so good.  So we change the handler to
+    // ignore the signal, and we explicitly check for the error when we
+    // need to.
+    //
+
+    sa.sa_handler = SIG_IGN;
+    if (sigaction(SIGPIPE, &sa, NULL) < 0) {
+        fprintf(stderr, "error setting SIGPIPE sigaction to SIG_IGN: %s", strerror(errno));
+        exit(1);
+    }
+}
+
+
+
+
 int main(int argc, char *argv[]) {
     int cal_fd = -1;
+
+
+    make_shutdowns_clean();
+
 
     while (1) {
         int r;
