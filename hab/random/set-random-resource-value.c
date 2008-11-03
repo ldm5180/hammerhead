b70bd3f55ae79883e38a55aa3a0389971fd6c5dc
seb <>
2008-11-03 19:23:44 +0000

    set the inital value of new datapoints better

--- hab/random/set-random-resource-value.c
+++ hab/random/set-random-resource-value.c
@@ -3,7 +3,7 @@
 // Copyright (C) 2008, Regents of the University of Colorado.
 //
 
-#define _ISOC99_SOURCE  // for isnormal()
+#define _GNU_SOURCE  // for isnormal()
 
 #include <errno.h>
 #include <math.h>
@@ -40,14 +40,7 @@ void randomize_buffer(void *buffer, int size) {
 
 void set_random_resource_value(bionet_resource_t* resource) {
     bionet_datapoint_t *datapoint;
-
-    datapoint = bionet_resource_get_datapoint_by_index(resource, 0);
-    if (datapoint == NULL) {
-        datapoint = bionet_datapoint_new(resource, "0", NULL);
-        bionet_resource_add_existing_datapoint(resource, datapoint);
-    } else {
-        bionet_datapoint_set_timestamp(datapoint, NULL);
-    }
+    bionet_datapoint_value_t value;
 
     switch (resource->data_type) {
         case BIONET_RESOURCE_DATA_TYPE_INVALID: {
@@ -56,51 +49,51 @@ void set_random_resource_value(bionet_resource_t* resource) {
         }
 
         case BIONET_RESOURCE_DATA_TYPE_BINARY: {
-            datapoint->value.binary_v = rand() % 2;
+            value.binary_v = rand() % 2;
             break;
         }
 
         case BIONET_RESOURCE_DATA_TYPE_UINT8: {
-            randomize_buffer(&datapoint->value.uint8_v, sizeof(uint8_t));
+            randomize_buffer(&value.uint8_v, sizeof(uint8_t));
             break;
         }
 
         case BIONET_RESOURCE_DATA_TYPE_INT8: {
-            randomize_buffer(&datapoint->value.int8_v, sizeof(int8_t));
+            randomize_buffer(&value.int8_v, sizeof(int8_t));
             break;
         }
 
         case BIONET_RESOURCE_DATA_TYPE_UINT16: {
-            randomize_buffer(&datapoint->value.uint16_v, sizeof(uint16_t));
+            randomize_buffer(&value.uint16_v, sizeof(uint16_t));
             break;
         }
 
         case BIONET_RESOURCE_DATA_TYPE_INT16: {
-            randomize_buffer(&datapoint->value.int16_v, sizeof(int16_t));
+            randomize_buffer(&value.int16_v, sizeof(int16_t));
             break;
         }
 
         case BIONET_RESOURCE_DATA_TYPE_UINT32: {
-            randomize_buffer(&datapoint->value.uint32_v, sizeof(uint32_t));
+            randomize_buffer(&value.uint32_v, sizeof(uint32_t));
             break;
         }
 
         case BIONET_RESOURCE_DATA_TYPE_INT32: {
-            randomize_buffer(&datapoint->value.int32_v, sizeof(int32_t));
+            randomize_buffer(&value.int32_v, sizeof(int32_t));
             break;
         }
 
         case BIONET_RESOURCE_DATA_TYPE_FLOAT: {
             do {
-                randomize_buffer(&datapoint->value.float_v, sizeof(float));
-            } while (!isnormal(datapoint->value.float_v));
+                randomize_buffer(&value.float_v, sizeof(float));
+            } while (!isnormal(value.float_v));
             break;
         }
 
         case BIONET_RESOURCE_DATA_TYPE_DOUBLE: {
             do {
-                randomize_buffer(&datapoint->value.double_v, sizeof(double));
-            } while (!isnormal(datapoint->value.double_v));
+                randomize_buffer(&value.double_v, sizeof(double));
+            } while (!isnormal(value.double_v));
             break;
         }
 
@@ -117,14 +110,27 @@ void set_random_resource_value(bionet_resource_t* resource) {
                 strcat(new_string, " ");
             }
             new_string[strlen(new_string) - 1] = '\0';
-            bionet_datapoint_value_from_string(datapoint, new_string);
-            return;
+            value.string_v = strdup(new_string);
+            if (value.string_v == NULL) {
+                printf("*** out of memory!\n");
+                return;
+            }
+            break;
         }
 
         default: {
             printf("*** unknown data type, skipping\n");
             return;
         }
+    }
+
+    datapoint = bionet_resource_get_datapoint_by_index(resource, 0);
+    if (datapoint == NULL) {
+        datapoint = bionet_datapoint_new(resource, &value, NULL);
+        bionet_resource_add_existing_datapoint(resource, datapoint);
+    } else {
+        bionet_datapoint_set_value(datapoint, &value);
+        bionet_datapoint_set_timestamp(datapoint, NULL);
     }
 }
 
