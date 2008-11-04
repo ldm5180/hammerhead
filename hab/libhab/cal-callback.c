71b311d128d185438fb01bf44cc2048d2ca9c4f3
seb <>
2008-11-04 23:03:58 +0000

    When a client subscribes, the hab now sends it all exiting matching info right away.

--- hab/libhab/cal-callback.c
+++ hab/libhab/cal-callback.c
@@ -29,7 +29,30 @@ void libhab_cal_callback(const cal_event_t *event) {
         }
 
         case CAL_EVENT_SUBSCRIBE: {
+            GSList *i;
+
             g_log(BIONET_LOG_DOMAIN, G_LOG_LEVEL_INFO, "CAL Subscribe event from '%s' for topic '%s'", event->peer_name, event->topic);
+
+            for (i = libhab_this->nodes; i != NULL; i = i->next) {
+                bionet_asn_buffer_t buf;
+                bionet_node_t *node = i->data;
+                int r;
+
+                if (libhab_cal_topic_matches(node->id, event->topic) != 0) continue;
+
+                r = bionet_node_to_asnbuf(node, &buf);
+                if (r != 0) {
+                    // an error has already been logged, and the buffer has been freed
+                    continue;
+                }
+
+                // "publish" the message to the newly connected subscriber (via sendto)
+                // cal_server.sendto takes the buf so we dont need to free it
+                cal_server.sendto(event->peer_name, buf.buf, buf.size);
+
+                g_log(BIONET_LOG_DOMAIN, G_LOG_LEVEL_INFO, "node %s matches topic %s", node->id, event->topic);
+            }
+            
             break;
         }
 
