0cae362d6027207fa7c1fcf013728a7d3b57b92e
seb <>
2008-11-04 23:54:56 +0000

    removed some more debug message

--- hab/libhab/cal-callback.c
+++ hab/libhab/cal-callback.c
@@ -14,24 +14,22 @@
 void libhab_cal_callback(const cal_event_t *event) {
     switch (event->type) {
         case CAL_EVENT_CONNECT: {
-            g_log(BIONET_LOG_DOMAIN, G_LOG_LEVEL_INFO, "CAL Connect event from '%s'", event->peer_name);
+            // we don't do anything with this
             break;
         }
 
         case CAL_EVENT_DISCONNECT: {
-            g_log(BIONET_LOG_DOMAIN, G_LOG_LEVEL_INFO, "CAL Disconnect event from '%s'", event->peer_name);
+            // we don't do anything with this
             break;
         }
 
         case CAL_EVENT_MESSAGE: {
-            g_log(BIONET_LOG_DOMAIN, G_LOG_LEVEL_INFO, "CAL Message event from '%s'", event->peer_name);
+            g_log(BIONET_LOG_DOMAIN, G_LOG_LEVEL_WARNING, "Got unexpected CAL Message event from '%s'", event->peer_name);
             break;
         }
 
         case CAL_EVENT_SUBSCRIBE: {
             GSList *i;
-
-            g_log(BIONET_LOG_DOMAIN, G_LOG_LEVEL_INFO, "CAL Subscribe event from '%s' for topic '%s'", event->peer_name, event->topic);
 
             for (i = libhab_this->nodes; i != NULL; i = i->next) {
                 bionet_asn_buffer_t buf;
@@ -49,8 +47,6 @@ void libhab_cal_callback(const cal_event_t *event) {
                 // "publish" the message to the newly connected subscriber (via sendto)
                 // cal_server.sendto takes the buf so we dont need to free it
                 cal_server.sendto(event->peer_name, buf.buf, buf.size);
-
-                g_log(BIONET_LOG_DOMAIN, G_LOG_LEVEL_INFO, "node %s matches topic %s", node->id, event->topic);
             }
             
             break;
