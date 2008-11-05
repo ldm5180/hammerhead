927c065e37942ae2e2cd87548fdeb98c707ea3f8
seb <>
2008-11-05 18:12:18 +0000

    better warning messages on insane arguments

--- util/is-valid-name-component.c
+++ util/is-valid-name-component.c
@@ -63,12 +63,12 @@ int bionet_is_valid_name_component_or_wildcard(const char *str) {
 
 int bionet_name_component_matches(const char *name_component, const char *pattern) {
     if (!bionet_is_valid_name_component(name_component)) {
-        g_log(BIONET_LOG_DOMAIN, G_LOG_LEVEL_WARNING, "bionet_name_component_matches(): invalid name-component passed in!");
+        g_log(BIONET_LOG_DOMAIN, G_LOG_LEVEL_WARNING, "bionet_name_component_matches(): invalid name-component '%s' passed in!", name_component);
         return 0;
     }
 
     if (!bionet_is_valid_name_component_or_wildcard(pattern)) {
-        g_log(BIONET_LOG_DOMAIN, G_LOG_LEVEL_WARNING, "bionet_name_component_matches(): invalid pattern passed in!");
+        g_log(BIONET_LOG_DOMAIN, G_LOG_LEVEL_WARNING, "bionet_name_component_matches(): invalid name-component-pattern '%s' passed in!", pattern);
         return 0;
     }
 
