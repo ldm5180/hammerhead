50579ba36fb527564e72a7929849a1c06d9bbee2
seb <>
2008-12-04 17:39:26 +0000

    log a warning on bogus arguments

--- cal/mdnssd-bip/shared/bip-net-clear.c
+++ cal/mdnssd-bip/shared/bip-net-clear.c
@@ -5,6 +5,11 @@
 
 
 void bip_net_clear(bip_peer_network_info_t *net) {
+    if (net == NULL) {
+        g_log(CAL_LOG_DOMAIN, G_LOG_LEVEL_WARNING, "bip_net_clear: NULL net passed in");
+        return;
+    }
+
     net->header_index = 0;
 
     if (net->buffer != NULL) {
