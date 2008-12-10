b679261c46d8623a3bb9658e8b4b45d3c64739f0
Lenny <lenny@lenny-ubuntu64>
2008-12-09 17:13:52 -0700

    more doxygen-ized comments for the util lib

--- util/bionet-resource.h
+++ util/bionet-resource.h
@@ -7,7 +7,10 @@
 #ifndef __BIONET_RESOURCE_H
 #define __BIONET_RESOURCE_H
 
-
+/**
+ * @file bionet-resource.h
+ * Functions for dealing with Bionet Hardware-Abstractor (HABs).
+ */
 
 
 #include <stdint.h>
@@ -19,12 +22,15 @@
 
 
 
-
+/**
+ * @typedef bionet_resource_flavor_t
+ * See the value in quotes for the string conversion value
+ */
 typedef enum {
-    BIONET_RESOURCE_FLAVOR_INVALID = -1,
-    BIONET_RESOURCE_FLAVOR_SENSOR,
-    BIONET_RESOURCE_FLAVOR_ACTUATOR,
-    BIONET_RESOURCE_FLAVOR_PARAMETER
+    BIONET_RESOURCE_FLAVOR_INVALID = -1, /**< "Invalid" */
+    BIONET_RESOURCE_FLAVOR_SENSOR, /**< "Sensor" */
+    BIONET_RESOURCE_FLAVOR_ACTUATOR, /**< "Actuator" */
+    BIONET_RESOURCE_FLAVOR_PARAMETER /**< "Parameter" */
 } bionet_resource_flavor_t;
 
 #define  BIONET_RESOURCE_FLAVOR_MIN  BIONET_RESOURCE_FLAVOR_SENSOR
@@ -53,26 +59,29 @@ typedef enum {
 
 
 
-//
-//  bionet_datapoint_value_t should always be initialized (all-bits-zero is valid)
-//
-
+/**
+ * @union bionet_datapoint_value_t
+ * @brief Union to describe how to read a value 
+ *
+ * @note bionet_datapoint_value_t should always be initialized 
+ * (all-bits-zero is valid)
+ */
 typedef union {
-    int binary_v;
+    int binary_v; /**< 0 or 1, TRUE or FALSE */
 
-    uint8_t  uint8_v;
-    int8_t   int8_v;
+    uint8_t  uint8_v; /**< 8-bit unsigned integer */
+    int8_t   int8_v; /**< 8-bit signed integer */
 
-    uint16_t uint16_v;
-    int16_t  int16_v;
+    uint16_t uint16_v; /**< 16-bit unsigned integer */
+    int16_t  int16_v; /**< 16-bit signed integer */
 
-    uint32_t uint32_v;
-    int32_t  int32_v;
+    uint32_t uint32_v; /**< 32-bit unsigned integer */
+    int32_t  int32_v; /**< 32-bit signed integer */
 
-    float float_v;
-    double double_v;
+    float float_v; /**< float */
+    double double_v; /**< double */
 
-    char *string_v;
+    char *string_v; /**< NULL-terminated array of characters */
 } bionet_datapoint_value_t;
 
 
@@ -107,32 +116,23 @@ struct bionet_resource {
 };
 
 
-
-
-//
-//
-//       NAME:  bionet_resource_new()
-//
-//   FUNCTION:  Allocates and initializes a new resource, from strings
-//              describing the resource.
-//
-//  ARGUMENTS:  All passed-in strings are considered the property of the
-//              caller.  The function duplicates what it needs, the caller
-//              is free to overwrite or free the strings on return from
-//              this function.
-//
-//              node: The Node that owns this Resource.  (optional)
-//
-//              data_type_str: The name of the data type of this Resource.
-//
-//              flavor_str: The flavor of this Resource.
-//
-//              id: The ID of this Resource.
-//
-//    RETURNS:  The Resource if all went well, NULL on error.
-//
-//
-
+/**
+ * @brief Allocates and initializes a new resource
+ *
+ * @param[in] node The Node that owns this Resource or NULL
+ * @param[in] data_type Data type of this Resource.
+ * @param[in] flavor The flavor of this Resource.
+ * @param[in] id The ID of this Resource.
+ *
+ * @return Pointer to a resource on success
+ * @retval NULL Error
+ * @retval >0 Success
+ *
+ * @note All passed-in strings are considered the property of the
+ *       caller.  The function duplicates what it needs, the caller
+ *       is free to overwrite or free the strings on return from
+ *       this function.
+ */
 bionet_resource_t *bionet_resource_new(
     const bionet_node_t *node,
     bionet_resource_data_type_t data_type,
@@ -140,6 +140,25 @@ bionet_resource_t *bionet_resource_new(
     const char *id
 );
 
+
+/**
+ * @brief Allocates and initializes a new resource, from strings
+ *        describing the resource.
+ *
+ * @param[in] node The Node that owns this Resource or NULL
+ * @param[in] data_type_str The name of the data type of this Resource as a string.
+ * @param[in] flavor_str The flavor of this Resource as a string.
+ * @param[in] id The ID of this Resource.
+ *
+ * @return Pointer to a resource on success
+ * @retval NULL Error
+ * @retval >0 Success
+ *
+ * @note All passed-in strings are considered the property of the
+ *       caller.  The function duplicates what it needs, the caller
+ *       is free to overwrite or free the strings on return from
+ *       this function.
+ */
 bionet_resource_t *bionet_resource_new_from_str(
     const bionet_node_t *node,
     const char *data_type_str,
@@ -148,91 +167,68 @@ bionet_resource_t *bionet_resource_new_from_str(
 );
 
 
-
-
-//
-//
-//       NAME:  bionet_resource_free()
-//
-//   FUNCTION:  Frees a Resource created with bionet_resource_new().
-//
-//  ARGUMENTS:  The Resource to free.
-//
-//    RETURNS:  Nothing.
-//
-//
-
+/**
+ * @brief Free a Resource created with bionet_resource_new().
+ *
+ * @param[in] resource The Resource to free.
+ */
 void bionet_resource_free(bionet_resource_t *resource);
 
 
-
-
-//
-//       NAME:  bionet_resource_flavor_to_string()
-//
-//   FUNCTION:  Renders a Resource-Flavor as an ASCII string.  The output
-//              will be one of "Sensor", "Actuator", or "Parameter".
-//
-//  ARGUMENTS:  The Resource Flavor to render.
-//
-//    RETURNS:  A pointer to the statically allocated string on success,
-//              NULL on error.
-//
-
+/**
+ * @brief Renders a Resource-Flavor as an ASCII string.
+ *
+ * See @ref bionet_resource_flavor_t for the string values
+ *
+ * @param[in] flavor The Resource Flavor to render.
+ *
+ * @return A pointer to the statically allocated string on success
+ * @retval NULL Error
+ */
 const char *bionet_resource_flavor_to_string(bionet_resource_flavor_t flavor);
 
 
-
-
-//
-//       NAME:  bionet_resource_flavor_from_string()
-//
-//   FUNCTION:  Tries to parse an ASCII string as a Resource-Flavor, and
-//              returns the Resource-Flavor.  Supported Resource-Flavors are
-//              "sensor", "actuator", and "parameter".  Case insensitive.
-//
-//  ARGUMENTS:  The string to parse the Resource-Flavor from.
-//
-//    RETURNS:  The Resource-Flavor (which may be
-//              BIONET_RESOURCE_FLAVOR_INVALID to indicate error).
-//
-
+/**
+ * @brief Tries to parse an ASCII string as a Resource-Flavor, and
+ *        returns the Resource-Flavor. See @ref bionet_resource_flavor_t for 
+ *        supported Resource-Flavors
+ *
+ * @param[in] flavor_string The string to parse the Resource-Flavor from.
+ *
+ * @return The Resource-Flavor
+ * @retval #BIONET_RESOURCE_FLAVOR_INVALID Error
+ *
+ * @note flavor_string is case-insensitive
+ */
 bionet_resource_flavor_t bionet_resource_flavor_from_string(const char *flavor_string);
 
 
-
-
-//
-//       NAME:  bionet_resource_data_type_to_string()
-//
-//   FUNCTION:  Renders a Resource-Data-Type as an ASCII string.
-//
-//  ARGUMENTS:  The Resource-Data-Type to render.
-//
-//    RETURNS:  A pointer to the statically allocated string on success,
-//              NULL on error.
-//
-
+/**
+ * @brief Renders a Resource-Data-Type as an ASCII string.
+ *
+ * See @ref bionet_resource_data_type_t for the string values
+ *
+ * @param[in] data_type The Resource-Data-Type to render.
+ *
+ * @return A pointer to the statically allocated string on success,
+ * @retval NULL Error
+ */
 const char *bionet_resource_data_type_to_string(bionet_resource_data_type_t data_type);
 
 
-
-
-//
-//       NAME:  bionet_resource_data_type_from_string()
-//
-//   FUNCTION:  Tries to parse an ASCII string as a Resource-Data-Type, and
-//              returns the Resource-Data-Type.  Case insensitive.
-//
-//  ARGUMENTS:  The string to parse the Resource-Flavor from.
-//
-//    RETURNS:  The Resource-Flavor (which may be
-//              BIONET_RESOURCE_FLAVOR_INVALID to indicate error).
-//
-
+/**
+ * @brief Tries to parse an ASCII string as a Resource-Data-Type, and
+ *        returns the Resource-Data-Type. See @ref bionet_resource_data_type_t 
+ *        for supported Resource-Data-Types
+ *
+ * @param[in] data_type_string The string to parse the Resource-Flavor from.
+ *
+ * @return The Resource-Flavor
+ * @retval #BIONET_RESOURCE_FLAVOR_INVALID Error
+ *
+ * @note data_type_string is case-insensitive
+ */
 bionet_resource_data_type_t bionet_resource_data_type_from_string(const char *data_type_string);
-
-
 
 
 /**
@@ -242,79 +238,121 @@ bionet_resource_data_type_t bionet_resource_data_type_from_string(const char *da
  * passed-in value and timestamp are copied to the Resource's first
  * Datapoint, and the Datapoint is marked dirty.
  *
- * @param resource  The Resource to set.
+ * @param[in] resource The Resource to set.
+ * @param[in] value The new Value.
+ * @param[in] timestamp The Timestamp, if NULL the current time will be used
  *
- * @param value The new Value.
- *
- * @param timestamp The new Timestamp (if NULL, the current time will be
- * used).
- *
- * @return 0 on success, -1 on failure.
+ * @retval 0 Success
+ * @retval -1 Failure
  */
-
-int bionet_resource_set(bionet_resource_t *resource, const bionet_datapoint_value_t *value, const struct timeval *timestamp);
-
-
-int bionet_resource_set_with_valuestr(bionet_resource_t *resource, const char *value_str, const struct timeval *timestamp);
+int bionet_resource_set(bionet_resource_t *resource, 
+			const bionet_datapoint_value_t *value, 
+			const struct timeval *timestamp);
 
 
+/**
+ * @brief Sets the value of a Resource from a string
+ *
+ * If the Resource has no Datapoint, one will be created.  Then the
+ * passed-in value and timestamp are copied to the Resource's first
+ * Datapoint, and the Datapoint is marked dirty.
+ *
+ * @param[in] resource The Resource to set.
+ * @param[in] value_str The new Value.
+ * @param[in] timestamp The Timestamp, if NULL the current time will be used
+ *
+ * @retval 0 Success
+ * @retval -1 Failure
+ */
+int bionet_resource_set_with_valuestr(bionet_resource_t *resource, 
+				      const char *value_str, 
+				      const struct timeval *timestamp);
 
 
-bionet_datapoint_t *bionet_resource_add_datapoint(bionet_resource_t *resource, const char *value_str, const struct timeval *timestamp);
-void bionet_resource_add_existing_datapoint(bionet_resource_t *resource, bionet_datapoint_t *new_datapoint);
+/**
+ * @brief Add a datapoint to a resource from a value string
+ *
+ * @param[in] resource The resource to which the datapoint is added
+ * @param[in] value_str The value for the datapoint
+ * @param[in] timestamp The Timestamp, if NULL the current time will be used
+ *
+ * @return Pointer to the new datapoint
+ * @retval NULL Failure
+ */
+bionet_datapoint_t *bionet_resource_add_datapoint(bionet_resource_t *resource, 
+						  const char *value_str, 
+						  const struct timeval *timestamp);
 
+
+
+/**
+ * @brief Add a datapoint to a resource
+ *
+ * @param[in] resource The resource to which the datapoint is added
+ * @param[in] new_datapoint The datapoint to add
+ */
+void bionet_resource_add_existing_datapoint(bionet_resource_t *resource, 
+					    bionet_datapoint_t *new_datapoint);
+
+
+/**
+ * @brief Get the number of datapoints in a resource
+ *
+ * @param[in] resource The resource
+ *
+ * @return The number of datapoints in a resource
+ * @retval -1 Invalid resource
+ */
 int bionet_resource_get_num_datapoints(const bionet_resource_t *resource);
+
+
+/**
+ * @brief Get a datapoint by index in the resource's list
+ *
+ * This can be used to iterate over a list of datapoints.
+ *
+ * @param[in] resource The resource
+ * @param[in] index Index of requested datapoint
+ *
+ * @return Pointer to a datapoint
+ * @retval NULL Failure
+ */
 bionet_datapoint_t *bionet_resource_get_datapoint_by_index(const bionet_resource_t *resource, unsigned int index);
 
 
+/**
+ * @brief Checks if a Resource matches a name specification.
+ *
+ * @see bionet_resource_matches_habtype_habid_nodeid_resourceid()
+ *
+ * @param[in] resource The Resource to check.
+ * @param[in] id The Resource-ID to check against.
+ *
+ * @note Any string may be replaced by the wildcard "*" to match all
+ *
+ * @retval 0 FALSE - The Resource does not match the ID
+ * @retval 1 TRUE - The Resource does match the ID
+ */
+int bionet_resource_matches_id(const bionet_resource_t *resource, 
+			       const char *id);
 
 
 /**
  * @brief Checks if a Resource matches a name specification.
  *
- * @note Also see the bionet_resource_matches_habtype_habid_nodeid_resourceid() function.
+ * @see bionet_resource_matches_id()
  *
- * @param resource The Resource to check.
+ * @param[in] resource The Resource to check.
+ * @param[in] hab_type The HAB-Type to check against. 
+ * @param[in] hab_id  The HAB-ID to check against.
+ * @param[in] node_id  The Node-ID to check against.
+ * @param[in] resource_id  The Resource-ID to check against.
  *
- * @param id The Resource-ID to check against.  id may be a regular
- *     Resource-ID, or it may be the wildcard "*" that matches all
- *     Resource-IDs.
+ * @note Any string may be replaced by the wildcard "*" to match all
  *
- * @return FALSE (zero) if the Resource does not match the ID, TRUE
- *     (non-zero) if it matches.
+ * @retval 0 FALSE - The Resource does not match the ID
+ * @retval 1 TRUE - The Resource does match the ID
  */
-
-int bionet_resource_matches_id(const bionet_resource_t *resource, const char *id);
-
-
-
-
-/**
- * @brief Checks if a Resource matches a name specification.
- *
- * @note Also see the bionet_resource_matches_id() function.
- *
- * @param resource  The Resource to check.
- *
- * @param hab_type  The HAB-Type to check against.  It may be a regular
- *     HAB-Type string, or it may be the wildcard "*" that matches all
- *     HAB-Types.
- *
- * @param hab_id  The HAB-ID to check against.  It may be a regular HAB-ID
- *     string, or it may be the wildcard "*" that matches all HAB-IDs.
- *
- * @param node_id  The Node-ID to check against.  It may be a regular
- *     Node-ID string, or it may be the wildcard "*" that matches all
- *     Node-IDs.
- *
- * @param resource_id  The Resource-ID to check against.  It may be a
- *     regular Resource-ID string, or it may be the wildcard "*" that
- *     matches all Resource-IDs.
- *
- * @return FALSE (zero) if the Resource does not match the ID, TRUE
- *     (non-zero) if it matches.
- */
-
 int bionet_resource_matches_habtype_habid_nodeid_resourceid(
     const bionet_resource_t *resource,
     const char *hab_type,
@@ -525,3 +563,8 @@ void bionet_datapoint_free(bionet_datapoint_t *datapoint);
 #endif // __BIONET_RESOURCE_H
 
 
+// Emacs cruft
+// Local Variables:
+// mode: C
+// c-file-style: "Stroustrup"
+// End:
