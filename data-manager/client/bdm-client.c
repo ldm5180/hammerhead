74d48577c28afa7017b0b93d38238c5b9db2a20e
Sebastian Kuzminsky <seb@highlab.com>
2008-12-15 14:09:00 -0700

    even better usage info

--- data-manager/client/bdm-client.c
+++ data-manager/client/bdm-client.c
@@ -78,7 +78,11 @@ void str_to_timeval(const char *str, struct timeval *tv) {
 
 void usage(void) {
     printf("usage: bdm-client [--server SERVER] ResourceNamePattern StartTime EndTime\n");
-    printf("    StartTime and EndTime are given in this format: \"YYYY-MM-DD HH:MM:SS\"\n");
+    printf("    StartTime and EndTime are given in this format: \"YYYY-MM-DD hh:mm:ss\"\n");
+    printf("        YYYY is the four-digit year, for example 2008\n");
+    printf("        MM is the two-digit month, with 01 meaning January and 12 meaning December\n");
+    printf("        DD is the two-digit day-of-the-month, with 01 meaning the first day\n");
+    printf("        hh:mm:ss is the 24-hour time\n");
 }
 
 
