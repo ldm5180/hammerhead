5b06b5b048bd3a31db2cb2e1e10d671977ecad0f
Sebastian Kuzminsky <seb@highlab.com>
2008-12-29 16:18:24 -0700

    reorg again to chase the under-development streams api

--- client/stream/bionet-stream.c
+++ client/stream/bionet-stream.c
@@ -80,30 +80,64 @@ void cb_new_node(bionet_node_t *node) {
 }
 
 
-
-
-void read_from_stream(bionet_stream_t *stream) {
-    fprintf(
-        stderr,
-        "reading from %s.%s.%s:%s\n",
+void cb_stream(bionet_stream_t *stream, void *buffer, int size) {
+    g_log(
+        "",
+        G_LOG_LEVEL_INFO,
+        "stream info: %s.%s.%s:%s got %d bytes",
         stream->node->hab->type,
         stream->node->hab->id,
         stream->node->id,
-        stream->id
+        stream->id,
+        size
     );
+}
 
-    pause();
 
-#if 0
-    int fd;
 
-    fprintf(stderr, "reading from %s\n", stream->id);
+
+// we're just going to list all the Bionet streams as they come and go
+void list_streams(void) {
+    bionet_register_callback_new_node(cb_new_node);
+    bionet_register_callback_lost_node(cb_lost_node);
+
+    bionet_subscribe_node_list_by_name("*.*.*");
+
+    while (1) {
+        bionet_read_with_timeout(NULL);
+    }
+}
+
+
+
+
+void read_from_stream(bionet_stream_t *stream) {
+    const char *stream_name;
+
+    stream_name = bionet_stream_get_name(stream);
+    if (stream_name == NULL) {
+        // an error has already been logged
+        exit(1);
+    }
+
+    fprintf(stderr, "reading from %s\n", stream_name);
+
+    // close stdin
+    fclose(stdin);
 
     // unbuffer stdout
     setvbuf(stdout, NULL, _IONBF, 0);
 
-    fd = *(int*)stream->user_data;
+    bionet_register_callback_stream(cb_stream);
 
+    bionet_subscribe_stream_by_name(stream_name);
+
+    while (1) {
+        bionet_read_with_timeout(NULL);
+        fprintf(stderr, "something happened\n");
+    }
+
+#if 0
     while (1) {
         int index;
         int bytes_remaining;
@@ -228,27 +262,6 @@ void deal_with_stream(bionet_stream_t *stream) {
             exit(1);
         }
     }
-
-
-#if 0
-    r = bionet_stream_connect(stream);
-    if (r < 0) {
-        fprintf(stderr, "error connecting to stream %s\n", stream_name);
-        exit(1);
-    }
-    stream->user_data = (int *)malloc(sizeof(int));
-    if (stream->user_data == NULL) {
-        fprintf(stderr, "out of memory!\n");
-        exit(1);
-    }
-    *(int*)stream->user_data = r;
-
-    if (stream->direction == BIONET_STREAM_DIRECTION_PRODUCER) {
-        read_from_stream(stream);
-    } else {
-        write_to_stream(stream);
-    }
-#endif
 }
 
 
@@ -256,6 +269,7 @@ void deal_with_stream(bionet_stream_t *stream) {
 
 int main(int argc, char *argv[]) {
     int i;
+    int r;
     char *stream_name = NULL;
 
     char *hab_type;
@@ -294,47 +308,41 @@ int main(int argc, char *argv[]) {
     g_log("", G_LOG_LEVEL_INFO, "connected to Bionet");
 
 
-    if (stream_name == NULL) {
-        bionet_register_callback_new_node(cb_new_node);
-        bionet_register_callback_lost_node(cb_lost_node);
-    } else {
+    // if no stream name was specified, we just list all the streams as they come and goo
+    if (stream_name == NULL) list_streams();
+
+
+    // 
+    // if we get here, we're going to connect to a stream
+    //
+
+    r = bionet_split_resource_name(stream_name, &hab_type, &hab_id, &node_id, &stream_id);
+    if (r != 0) exit(1);
+
+
+    // we need to subscribe to the node-list so we discover the Stream, so we can learn if it's a Producer or Consumer
+    {
+        char node_name[(3 * BIONET_NAME_COMPONENT_MAX_LEN) + 3];
         int r;
 
-        r = bionet_split_resource_name(stream_name, &hab_type, &hab_id, &node_id, &stream_id);
-        if (r != 0) {
-            g_log("", G_LOG_LEVEL_WARNING, "error splitting stream name '%s'", stream_name);
+        r = snprintf(node_name, sizeof(node_name), "%s.%s.%s", hab_type, hab_id, node_id);
+        if ((r >= sizeof(node_name)) || (r < 5)) {
+            fprintf(stderr, "error making node name for node-list subscription\n");
             exit(1);
         }
+
+        bionet_subscribe_node_list_by_name(node_name);
     }
 
 
-    bionet_subscribe_node_list_by_name("*.*.*");
-
-
     while (1) {
-        int r;
-        fd_set readers;
         bionet_stream_t *stream;
 
-        FD_ZERO(&readers);
-        FD_SET(bionet_fd, &readers);
-
-        r = select(bionet_fd + 1, &readers, NULL, NULL, NULL);
-
-        if ((r < 0) && (errno != EINTR)) {
-            g_log("", G_LOG_LEVEL_WARNING, "error from select: %s", strerror(errno));
-            g_usleep(1000*1000);
-            continue;
-        }
-
-        bionet_read();
-
-        if (stream_name == NULL) continue;
+        bionet_read_with_timeout(NULL);
 
         stream = bionet_cache_lookup_stream(hab_type, hab_id, node_id, stream_id);
         if (stream == NULL) continue;
 
-        printf("found stream %s!\n", stream_name);
         deal_with_stream(stream);
         // wont return
     } 
