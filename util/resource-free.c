50ab7f3038b873e24586b12f13ce09882d45d00a
Sebastian Kuzminsky <seb@highlab.com>
2009-01-08 14:39:36 -0700

    fix a memory leak

--- util/resource-free.c
+++ util/resource-free.c
@@ -23,6 +23,10 @@ void bionet_resource_free(bionet_resource_t *resource) {
         g_log(BIONET_LOG_DOMAIN, G_LOG_LEVEL_WARNING, "bionet_resource_free() called with Resource %s, user_data is not NULL, leaking memory now", resource->id);
     }
 
+    if (resource->id != NULL) {
+        free(resource->id);
+    }
+
     // free all the datapoints
     while(resource->datapoints->len > 0) {
         bionet_datapoint_t *d = g_ptr_array_remove_index_fast(resource->datapoints, 0);
