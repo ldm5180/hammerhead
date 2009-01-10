9821dfba74ffd089491991713f859d8bd85ebe1a
Lenny Maiorani <lenny@colorado.edu>
2009-01-09 22:49:29 -0700

    switched the python version back to bdm-integrity.sh and the C version 'run'

--- test/tests/101.random-hab-and-bdm/bdm-integrity.sh
+++ test/tests/101.random-hab-and-bdm/bdm-integrity.sh
@@ -15,28 +15,24 @@ mv bdm.db $ORIG_DIR/.
 mv bdm.db-old bdm.db
 cd $ORIG_DIR
 echo "Starting BDM server..."
-../../../data-manager/server/bionet-data-manager -f $ORIG_DIR/bdm.db &
-../../../client/watcher/bionet-watcher > watcher.out &
+../../../data-manager/server/bionet-data-manager -f ./bdm.db 2> /dev/null &
 sleep 1
 
 #run a Random HAB and output the test data here
-cd ../../../hab/random
+cd ../../../hab/random-py
 echo "Running Random-HAB..."
-./random-hab -i tester1 --output-mode bdm-client 2> $ORIG_DIR/random.out &
-sleep 35
-killall lt-random-hab 
-cd $ORIG_DIR
+./random-hab.py -i tester1 -t $ORIG_DIR/random.out -l 30 > /dev/null 2> /dev/null
+sleep 1
 
 #grab data from the BDM
 YEAR=`date +%Y`
 cd ../../../data-manager/scripts
 echo "Running BDM-Client..."
-./bdm-client.py -o chrono -s localhost -b "$YEAR-01-01 00:00:00" -e "$YEAR-12-31 12:59:59" -r "*.*.*:*" > $ORIG_DIR/bdm.out
+./bdm-client.py -o chrono -s localhost -b "$YEAR-01-01 00:00:00" -e "$YEAR-12-31 12:59:59" -r "RANDOM.*.*:*" > $ORIG_DIR/bdm.out
 
 #stop the BDM
 echo "Killing BDM server..."
 killall lt-bionet-data-manager
-killall lt-bionet-watcher
 
 cd $ORIG_DIR
 
