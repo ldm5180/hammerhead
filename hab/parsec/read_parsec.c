a4dc521275ef19d50cc771abc85fe9cc1721f726
Shea Williams <Shea.Williams@colorado.edu>
2009-01-13 15:40:48 -0600

    Or else just update the resources for the existing node.

--- hab/parsec/read_parsec.c
+++ hab/parsec/read_parsec.c
@@ -24,6 +24,7 @@ void read_parsec(int fd) {
   struct timeval timestamp;
 
   bionet_node_t node;
+  bionet_resource_t resource;
 
   size = recvfrom(fd, buffer, BUFFER_SIZE, 0, 0, 0);
   timestamp = gettimeofday(&timestamp, NULL);
@@ -39,7 +40,6 @@ void read_parsec(int fd) {
   node = bionet_hab_get_node_by_name(parsec_hab, id);
   if (node == NULL) {
 	// Add the node
-	bionet_resource_t resource;
 	node = bionet_node_new(parsec_hab, id);
 	resource = bionet_resource_new(node, 
 								   BIONET_RESOURCE_DATA_TYPE_FLOAT, 
@@ -82,6 +82,32 @@ void read_parsec(int fd) {
 	}
 
   } else {
-	//
+	// Get the node and update
+
+	node = bionet_hab_get_node_by_id(parsec_hab, id);
+
+	// Range
+	resource = bionet_node_get_resource_by_id(node, "Range");
+
+	if (resource == NULL) {
+	  g_error("Failed to get Range resource for node %d", id);
+	}
+
+	if (bionet_set_resource_float(resource, range, &timestamp)) {
+	  g_error("Failed to set Range resource for node %d", id);
+	  return;
+	}
+
+	// Temp
+	resource = bionet_node_get_resource_by_id(node, "Temperature");
+
+	if (resource == NULL) {
+	  g_error("Failed to get Temperature resource for node %d", id);
+	}
+
+	if (bionet_set_resource_float(resource, temp, &timestamp)) {
+	  g_error("Failed to set Temperature resource for node %d", id);
+	  return;
+	}
   }
 }
