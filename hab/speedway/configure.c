6ce5934e18d93f7b97d6b3fa33241b873baf04d5
Sebastian Kuzminsky <seb@highlab.com>
2009-01-14 12:07:59 -0600

    only whitespace changes for prettification

--- hab/speedway/configure.c
+++ hab/speedway/configure.c
@@ -10,33 +10,25 @@
 #include "ltkc.h"
 #include "speedway.h"
 
-/*
- * The steps done here are:
- * 	- Clear (scrub) the reader configuration.
- * 	- Configure for what we want to do.
- */
 
 int speedway_configure() {
-	int r = 0;
+    int r = 0;
 
-	r = scrubConfiguration();	
+    r = scrubConfiguration();	
+    if (r != 0) {
+        g_warning("scrubConfiguration error");
+    }
 
-	if (r != 0) {
-		g_warning("scrubConfiguration error");
-	}
+    r = addROSpec();
+    if (r != 0) {
+        g_warning("addROSpec error");
+    }
 
-	r = addROSpec();
+    r = enableROSpec();
+    if (r != 0) {
+        g_warning("enableROSpec error");
+    }
 
-	if (r != 0) {
-		g_warning("addROSpec error");
-	}
-
-	r = enableROSpec();
-
-	if (r != 0) {
-		g_warning("enableROSpec error");
-	}
-
-	return r;
+    return r;
 }
 
