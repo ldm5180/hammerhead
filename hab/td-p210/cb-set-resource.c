41cb79fbab5a59b199115c1c65869cb7fac93c1f
Sebastian Kuzminsky <seb@highlab.com>
2009-01-15 12:11:42 -0600

    initial commit of Garys code

---
+++ hab/td-p210/cb-set-resource.c
@@ -0,0 +1,11 @@
+
+#include <hardware-abstractor.h>
+
+void cb_set_resource(const char *node_id, const char *resource_id, 
+		const char *value) {
+
+    g_message("not implemented yet");
+
+	return;
+}
+
---
+++ hab/td-p210/make-shutdowns-clean.c
@@ -0,0 +1,54 @@
+
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+#if defined(LINUX) || defined(MAC_OSX)
+    #include <signal.h>
+#endif
+
+#include <glib.h>
+
+#include "td-radio.h"
+
+#if defined(LINUX) || defined(MAC_OSX)
+static void exit_signal_handler(int signal_number) {
+    exit(0);
+}
+#endif
+
+
+void make_shutdowns_clean(void) {
+
+#if defined(LINUX) || defined(MAC_OSX)
+/*
+    struct sigaction sa;
+
+    sa.sa_handler = exit_signal_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+
+    if (sigaction(SIGABRT, &sa, NULL) < 0) {
+        g_log("", G_LOG_LEVEL_ERROR, "sigaction(SIGABORT, ...): %s", 
+			strerror(errno));
+
+        exit(1);
+    }
+
+    if (sigaction(SIGINT, &sa, NULL) < 0) {
+        g_log("", G_LOG_LEVEL_ERROR, "sigaction(SIGINT, ...): %s", 
+			strerror(errno));
+
+        exit(1);
+    }
+
+    if (sigaction(SIGTERM, &sa, NULL) < 0) {
+        g_log("", G_LOG_LEVEL_ERROR, "sigaction(SIGTERM, ...): %s", 
+			strerror(errno));
+
+        exit(1);
+    }
+*/
+#endif
+}
+
