e69152017f27d0be5378eafea4c0adf028d47ab7
Sebastian Kuzminsky <seb@highlab.com>
2009-01-27 09:28:40 -0700

    accept --id on the command-line (makes testing easier)

--- hab/streamy/streamy-hab.c
+++ hab/streamy/streamy-hab.c
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <glib.h>
 
@@ -24,9 +25,31 @@ GMainLoop *main_loop = NULL;
 
 int main(int argc, char *argv[]) {
     int bionet_fd;
+    char *id = NULL;
 
 
     g_log_set_default_handler(bionet_glib_log_handler, NULL);
+
+
+    // 
+    // handle command-line arguments
+    //
+
+    {
+        int i;
+
+        for (i = 1; i < argc; i ++) {
+            if (strcmp(argv[i], "--id") == 0) {
+                i ++;
+                id = argv[i];
+                break;
+
+            } else {
+                fprintf(stderr, "unknown command-line argument: %s\n", argv[i]);
+                exit(1);
+            }
+        }
+    }
 
 
     //
@@ -37,7 +60,7 @@ int main(int argc, char *argv[]) {
     hab_register_callback_stream_unsubscription(cb_stream_unsubscription);
     hab_register_callback_stream_data(cb_stream_data);
 
-    this_hab = bionet_hab_new("Streamy", NULL);
+    this_hab = bionet_hab_new("Streamy", id);
 
     bionet_fd = hab_connect(this_hab);
     if (bionet_fd < 0) {
