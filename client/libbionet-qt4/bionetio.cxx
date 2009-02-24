86512011f4e37807d3a08115d13961b53304ff5b
seb@highlab.com <>
2009-02-24 15:14:09 -0700

    updated copyright notices from Kendria

--- client/libbionet-qt4/bionetio.cxx
+++ client/libbionet-qt4/bionetio.cxx
@@ -1,21 +1,7 @@
 
-//
-// Copyright (C) 2004-2008, Regents of the University of Colorado.
-// This work was supported by NASA contracts NNJ05HE10G and NNC06CB40C.
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of GNU General Public License version 2, as
-// published by the Free Software Foundation.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
-//
+// Copyright (c) 2008-2009, Regents of the University of Colorado.
+// This work was supported by NASA contracts NNJ05HE10G, NNC06CB40C, and
+// NNC07CB47C.
 
 
 #include "bionetio.h"
