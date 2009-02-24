b82c82a391f4ae8708b2621945c511534a2bbf47
seb@highlab.com <>
2009-02-24 15:42:52 -0700

    copyright notices from Kendria

--- configure-mdacs.sh
+++ configure-mdacs.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+#
+# Copyright (c) 2008-2009, Regents of the University of Colorado.
+# This work was supported by NASA contracts NNJ05HE10G, NNC06CB40C, and
+# NNC07CB47C.
+#
 
 ./configure --enable-static --disable-shared -build=i386 --host=none-uclinux-uclibc CC='ucfront microblaze-uclinux-gcc' LDFLAGS='-Wl, -elf2flt' GLIB20_CFLAGS=-I$PETALINUX/software/petalinux-dist/glib/include/glib-2.0 GLIB20_LIBS=$PETALINUX/software/petalinux-dist/glib/lib/libglib-2.0.la DBUS_CFLAGS="-I$PETALINUX/software/petalinux-dist/dbus/include/dbus-1.0 -I$PETALINUX/software/petalinux-dist/dbus/lib/dbus-1.0/include" DBUS_LIBS=$PETALINUX/software/petalinux-dist/dbus/lib/libdbus-1.la AVAHI_COMPAT_LIBDNS_SD_CFLAGS=-I$PETALINUX/software/petalinux-dist/avahi/include/avahi-compat-libdns_sd AVAHI_COMPAT_LIBDNS_SD_LIBS=$PETALINUX/software/petalinux-dist/avahi/lib/libdns_sd.la LIBS=$PETALINUX/software/petalinux-dist/dbus/lib/libdbus-1.a
 
