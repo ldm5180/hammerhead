23aa29bb3bb6d8950583e8b577e04a79fc857a5c
seb@highlab.com <>
2009-02-24 16:03:51 -0700

    copyright notice updates

--- easy-build-mdacs.sh
+++ easy-build-mdacs.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+#
+# Copyright (c) 2008-2009, Regents of the University of Colorado.
+# This work was supported by NASA contracts NNJ05HE10G, NNC06CB40C, and
+# NNC07CB47C.
+#
+
 
 cd platforms/petalinux/user-apps
 ./easy_build.sh
