473dda7937f5a972f7fe4346a54a33a55dbf3963
Brian Marshall <brian.marshall@colorado.edu>
2009-04-16 15:50:38 -0600

    Fix check for may by not relying on proc auto-assigned hab-type

--- check/libutil/check-libutil-hab-tests.c
+++ check/libutil/check-libutil-hab-tests.c
@@ -19,48 +19,38 @@
 #include "bionet-util.h"
 #include "check-libutil-hab-tests.h"
 
-
 START_TEST (test_libutil_hab_new_0) {
     bionet_hab_t * hab;
     const char * hab_type;
     const char * hab_id;
-    char program_name[500];
     char hostname[250];
     char * tmp;
 
-    hab = bionet_hab_new(NULL, NULL);
+    const char * mytype = "test123";
+
+    hab = bionet_hab_new(mytype, NULL);
     fail_unless(NULL != hab, "Failed to get a new HAB: %m\n");
     
     hab_type = bionet_hab_get_type(hab);
     hab_id = bionet_hab_get_id(hab);
 
-    int fd, r;
-    fd = open("/proc/self/cmdline", O_RDONLY);
-    fail_unless(fd, "Error opening /proc/self/cmdline: %m");
-    r = read(fd, program_name, sizeof(program_name) - 1);
-    fail_if(r <= 0, "Error reading /proc/self/cmdline: %m");
-    program_name[r] = '\0';  // this is redundant, because the /proc file already 
-                             //contains the NUL, but it makes Coverity happy
-    while ((tmp = memchr(program_name, '/', strlen(program_name))) != NULL) {
-        int new_len = strlen(tmp+1);
-        memmove(program_name, tmp+1, new_len);
-        program_name[new_len] = '\0';
-    }
+    fail_if(strcmp(hab_type, mytype), 
+	    "HAB-Type should be %s, but is %s", 
+	    mytype, hab_type);
 
-    for (tmp = program_name; *tmp != '\0'; tmp ++) {
+    int r;
+    r = gethostname(hostname, sizeof(hostname));
+    fail_if(r < 0, "error getting hostname: %m");
+
+    for (tmp = hostname; *tmp != '\0'; tmp ++) {
         if (!isalnum(*tmp) && *tmp != '-') {
             *tmp = '-';
         }
     }
-    fail_if(strcmp(hab_type, program_name), 
-	    "HAB-Type should be %s, but is %s", 
-	    program_name, hab_type);
 
-    r = gethostname(hostname, sizeof(hostname));
-    fail_if(r < 0, "error getting hostname: %m");
 
     fail_if(strcmp(hab_id, hostname), "HAB-ID should be %s, but is %s",
-	    hostname, program_name);
+	    hostname, hab_id);
 } END_TEST /* test_libutil_hab_new_0 */
 
 
