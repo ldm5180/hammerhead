2e6d26fc55267655ccf4a9fe09efb60d56f1c53f
Lenny <lenny@lenny-centos5-xilinx.colorado.edu>
2009-05-05 16:16:22 -0600

    add gthread and fix dependencies with openssl

--- configure-mdacs.sh
+++ configure-mdacs.sh
@@ -20,12 +20,12 @@
     CC='ucfront microblaze-uclinux-gcc' \
     LDFLAGS='-Wl, -elf2flt' \
     GLIB20_CFLAGS=-I$PETALINUX/software/petalinux-dist/glib/include/glib-2.0 \
-    GLIB20_LIBS=$PETALINUX/software/petalinux-dist/glib/lib/libglib-2.0.la \
+    GLIB20_LIBS="$PETALINUX/software/petalinux-dist/glib/lib/libglib-2.0.la $PETALINUX/software/petalinux-dist/glib/lib/libgthread-2.0.la" \
     DBUS_CFLAGS="-I$PETALINUX/software/petalinux-dist/dbus/include/dbus-1.0 -I$PETALINUX/software/petalinux-dist/dbus/lib/dbus-1.0/include" \
     DBUS_LIBS=$PETALINUX/software/petalinux-dist/dbus/lib/libdbus-1.la \
     AVAHI_COMPAT_LIBDNS_SD_CFLAGS=-I$PETALINUX/software/petalinux-dist/avahi/include/avahi-compat-libdns_sd \
     AVAHI_COMPAT_LIBDNS_SD_LIBS=$PETALINUX/software/petalinux-dist/avahi/lib/libdns_sd.la \
     LIBS=$PETALINUX/software/petalinux-dist/dbus/lib/libdbus-1.a \
     OPENSSL_CFLAGS=-I$PETALINUX/software/petalinux-dist/openssl/include \
-    OPENSSL_LIBS="$PETALINUX/software/petalinux-dist/openssl/libcrypto.a $PETALINUX/software/petalinux-dist/openssl/libssl.a"
+    OPENSSL_LIBS="$PETALINUX/software/petalinux-dist/openssl/lib/libssl.a $PETALINUX/software/petalinux-dist/openssl/lib/libcrypto.a"
 
