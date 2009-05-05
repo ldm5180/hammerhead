614da5be2d1df9760c37e7079b0a0682e409e011
Lenny <lenny@lenny-centos5-xilinx.colorado.edu>
2009-05-05 09:40:13 -0600

    libiconv is not needed afterall.

--- platforms/petalinux/user-apps/libiconv-1.13/easy_configure.sh
+++
@@ -1,1 +0,0 @@
- ./configure -C --cache-file=mb.cache --enable-static --disable-shared --prefix=$PETALINUX/software/petalinux-dist/libiconv --build=i386 --host=none-uclinux-uclibc CC='ucfront microblaze-uclinux-gcc' LDFLAGS='-Wl, -elf2flt' 
