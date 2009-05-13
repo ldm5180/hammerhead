9f5a67c3a4431a8da48d2394e857734fa632fb6f
Lenny Maiorani <lenny@colorado.edu>
2009-05-13 11:04:00 -0600

    add the bip-peer-is-secure.c file so that the build actually works

---
+++ cal/mdnssd-bip/shared/bip-peer-is-secure.c
@@ -0,0 +1,49 @@
+
+// Copyright (c) 2008-2009, Regents of the University of Colorado.
+// This work was supported by NASA contracts NNJ05HE10G, NNC06CB40C, and
+// NNC07CB47C.
+
+
+#include <ctype.h>
+#include <errno.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <arpa/inet.h>
+
+#include <netinet/in.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <glib.h>
+#include <dns_sd.h>
+
+#include "cal-mdnssd-bip.h"
+
+
+
+
+int bip_peer_is_secure(bip_peer_t *peer) {
+    bip_peer_network_info_t *net;
+
+    if (peer == NULL) {
+        g_log(CAL_LOG_DOMAIN, G_LOG_LEVEL_WARNING, "bip_peer_is_secure: NULL peer passed in");
+        return 0;
+    }
+
+    net = bip_peer_get_connected_net(peer);
+    if (net == NULL) {
+	return 0;
+    }
+
+    if (BIP_SEC_REQ == net->security_status) {
+	return 1;
+    }
+
+    return 0;
+}
+
