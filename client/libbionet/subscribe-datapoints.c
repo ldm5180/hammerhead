408528171b13105f1bd4cdb5d150519ce5eae0a2
Marek Sotola <sotola@colorado.edu>
2009-06-03 16:51:36 -0600

    populate the libbionet_datapoint_subscriptions list so we can check against it later when we get new node messages

--- client/libbionet/subscribe-datapoints.c
+++ client/libbionet/subscribe-datapoints.c
@@ -6,6 +6,7 @@
 
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include <glib.h>
@@ -18,6 +19,29 @@ int bionet_subscribe_datapoints_by_habtype_habid_nodeid_resourceid(const char *h
     int r;
     char publisher[BIONET_NAME_COMPONENT_MAX_LEN * 2];
     char topic[(BIONET_NAME_COMPONENT_MAX_LEN * 2) + 2];  // the +2 is one for the leading "D " to specify the subscription family
+    libbionet_datapoint_subscription_t *new_datapoint_sub;
+
+    new_datapoint_sub = calloc(1, sizeof(libbionet_datapoint_subscription_t));
+    if (new_datapoint_sub == NULL)
+        goto fail0;
+
+    new_datapoint_sub->hab_type = strdup(hab_type);
+    if (new_datapoint_sub->hab_type == NULL) 
+        goto fail1;
+
+    new_datapoint_sub->hab_id = strdup(hab_id);
+    if (new_datapoint_sub->hab_id == NULL) 
+        goto fail2;
+
+    new_datapoint_sub->node_id = strdup(node_id);
+    if (new_datapoint_sub->node_id == NULL) 
+        goto fail3;
+
+    new_datapoint_sub->resource_id = strdup(resource_id);
+    if (new_datapoint_sub->resource_id == NULL) 
+        goto fail4;
+
+    libbionet_datapoint_subscriptions = g_slist_prepend(libbionet_datapoint_subscriptions, new_datapoint_sub);
 
     r = snprintf(publisher, sizeof(publisher), "%s.%s", hab_type, hab_id);
     if (r >= sizeof(publisher)) {
@@ -36,6 +60,22 @@ int bionet_subscribe_datapoints_by_habtype_habid_nodeid_resourceid(const char *h
     if (!r) return -1;
 
     return 0;
+
+fail4:
+    free(new_datapoint_sub->node_id);
+
+fail3:
+    free(new_datapoint_sub->hab_id);
+
+fail2:
+    free(new_datapoint_sub->hab_type);
+
+fail1:
+    free(new_datapoint_sub);
+
+fail0:
+    g_log(BIONET_LOG_DOMAIN, G_LOG_LEVEL_WARNING, "bionet_subscribe_datapoints_by_habtype_habid_nodeid_resourceid(): out of memory");
+    return -1;
 }
 
 
