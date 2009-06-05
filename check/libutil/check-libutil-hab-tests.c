09dc018facda5318ca47d47bf6797c61b2bb611c
Lenny Maiorani <lenny@colorado.edu>
2009-06-05 10:09:40 -0600

    adding check unit tests to more completely test bionet_hab_new()

--- check/libutil/check-libutil-hab-tests.c
+++ check/libutil/check-libutil-hab-tests.c
@@ -19,6 +19,9 @@
 #include "bionet-util.h"
 #include "check-libutil-hab-tests.h"
 
+/*
+ * hab_new(type, NULL)
+ */
 START_TEST (test_libutil_hab_new_0) {
     bionet_hab_t * hab;
     const char * hab_type;
@@ -54,6 +57,115 @@ START_TEST (test_libutil_hab_new_0) {
 } END_TEST /* test_libutil_hab_new_0 */
 
 
+/*
+ * hab_new(NULL, NULL)
+ */
+START_TEST (test_libutil_hab_new_1) {
+    bionet_hab_t * hab;
+    const char * hab_type;
+    const char * hab_id;
+    char * tmp;
+
+    const char * myid = "test123";
+    char program_name[500];
+    int fd, r;
+ 
+    hab = bionet_hab_new(NULL, myid);
+    fail_unless(NULL != hab, "Failed to get a new HAB: %m\n");
+    
+    hab_type = bionet_hab_get_type(hab);
+    hab_id = bionet_hab_get_id(hab);
+
+    fd = open("/proc/self/cmdline", O_RDONLY);
+    fail_if(fd < 0,
+	    "error opening /proc/self/cmdline (%m)\n");
+
+    r = read(fd, program_name, sizeof(program_name) - 1);
+    close(fd);
+    fail_if (r <= 0,
+	     "error reading /proc/self/cmdline (%m)\n");
+
+    while ((tmp = memchr(program_name, '/', strlen(program_name))) != NULL) {
+        int new_len = strlen(tmp+1);
+        memmove(program_name, tmp+1, new_len);
+        program_name[new_len] = '\0';
+    }
+
+    for (tmp = program_name; *tmp != '\0'; tmp ++) {
+        if (!isalnum(*tmp) && *tmp != '-') {
+            *tmp = '-';
+        }
+    }
+
+    fail_if(strcmp(hab_type, program_name), 
+	    "HAB-Type should be %s, but is %s\n", 
+	    program_name, hab_type);
+
+    fail_if(strcmp(myid, hab_id),
+	    "HAB-ID should be %s, but it is %s\n",
+	    myid, hab_id);
+} END_TEST /* test_libutil_hab_new_1 */
+
+
+/*
+ * hab_new(type, NULL)
+ */
+START_TEST (test_libutil_hab_new_2) {
+    bionet_hab_t * hab;
+    const char * hab_type;
+    const char * hab_id;
+    char hostname[250];
+    char * tmp;
+
+    char program_name[500];
+    int fd, r;
+ 
+    hab = bionet_hab_new(NULL, NULL);
+    fail_unless(NULL != hab, "Failed to get a new HAB: %m\n");
+    
+    hab_type = bionet_hab_get_type(hab);
+    hab_id = bionet_hab_get_id(hab);
+
+    fd = open("/proc/self/cmdline", O_RDONLY);
+    fail_if(fd < 0,
+	    "error opening /proc/self/cmdline (%m)\n");
+
+    r = read(fd, program_name, sizeof(program_name) - 1);
+    close(fd);
+    fail_if (r <= 0,
+	     "error reading /proc/self/cmdline (%m)\n");
+
+    while ((tmp = memchr(program_name, '/', strlen(program_name))) != NULL) {
+        int new_len = strlen(tmp+1);
+        memmove(program_name, tmp+1, new_len);
+        program_name[new_len] = '\0';
+    }
+
+    for (tmp = program_name; *tmp != '\0'; tmp ++) {
+        if (!isalnum(*tmp) && *tmp != '-') {
+            *tmp = '-';
+        }
+    }
+
+    fail_if(strcmp(hab_type, program_name), 
+	    "HAB-Type should be %s, but is %s\n", 
+	    program_name, hab_type);
+
+    r = gethostname(hostname, sizeof(hostname));
+    fail_if(r < 0, "error getting hostname: %m");
+
+    for (tmp = hostname; *tmp != '\0'; tmp ++) {
+        if (!isalnum(*tmp) && *tmp != '-') {
+            *tmp = '-';
+        }
+    }
+
+    fail_if(strcmp(hostname, hab_id),
+	    "HAB-ID should be %s, but it is %s\n",
+	    hostname, hab_id);
+} END_TEST /* test_libutil_hab_new_2 */
+
+
 void libutil_hab_tests_suite(Suite *s)
 {
     TCase *tc = tcase_create("bionet_hab_new()");
@@ -61,6 +173,8 @@ void libutil_hab_tests_suite(Suite *s)
 
 
     tcase_add_test(tc, test_libutil_hab_new_0);
+    tcase_add_test(tc, test_libutil_hab_new_1);
+    tcase_add_test(tc, test_libutil_hab_new_2);
 
 
     return;
