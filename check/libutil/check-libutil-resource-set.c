9b02de690a0fbcbe80056781fcae0008360f7556
Sebastian Kuzminsky <seb@highlab.com>
2009-06-23 16:17:01 -0600

    add the unit tests referenced by the previous makefile commit...

---
+++ check/libutil/check-libutil-resource-set.c
@@ -0,0 +1,51 @@
+
+// Copyright (c) 2008-2009, Regents of the University of Colorado.
+// This work was supported by NASA contracts NNJ05HE10G, NNC06CB40C, and
+// NNC07CB47C.
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <check.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "check-common.h"
+#include "bionet-util.h"
+#include "check-libutil-hab-tests.h"
+
+
+START_TEST (test_libutil_resource_set_str_0) {
+    bionet_resource_t *resource;
+    int r;
+
+    resource = bionet_resource_new(NULL, BIONET_RESOURCE_DATA_TYPE_STRING, BIONET_RESOURCE_FLAVOR_PARAMETER, "FancyString");
+    fail_if(resource == NULL, "failed to create a perfectly normal resource\n");
+
+    r = bionet_resource_set_str(resource, "blabber number one", NULL);
+    fail_if(r != 0, "failed to set the resource string value the first time\n");
+
+    r = bionet_resource_set_str(resource, "blather number two", NULL);
+    fail_if(r != 0, "failed to set the resource string value the second time\n");
+
+    bionet_resource_free(resource);
+
+} END_TEST
+
+
+
+
+void libutil_resource_set_suite(Suite *s) {
+    TCase *tc = tcase_create("bionet_resource_set");
+    suite_add_tcase(s, tc);
+
+    tcase_add_test(tc, test_libutil_resource_set_str_0);
+
+    return;
+}
+
---
+++ check/libutil/check-libutil-resource-set.h
@@ -0,0 +1,26 @@
+
+// Copyright (c) 2008-2009, Regents of the University of Colorado.
+// This work was supported by NASA contracts NNJ05HE10G, NNC06CB40C, and
+// NNC07CB47C.
+
+
+#ifndef CHECK_LIBUTIL_RESOURCE_SET_H
+#define CHECK_LIBUTIL_RESOURCE_SET_H
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include <check.h>
+
+
+void libutil_resource_set_suite(Suite *s);
+
+
+
+
+#endif  //  CHECK_LIBUTIL_RESOURCE_SET_H
+
