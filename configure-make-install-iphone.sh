df056c40fcd8b8835e9f7d8c6d5422d2c285b26e
Brian Marshall <brian.marshall@colorado.edu>
2009-07-15 10:01:27 -0600

    Fix iphone(sim) build by pre-defining path to GLIB.

--- configure-make-install-iphone.sh
+++ configure-make-install-iphone.sh
@@ -57,6 +57,11 @@ autoreconf --force --install
 #export AR="$XPORTROOT/bin/$XHOST-ar"
 #export NM="$XPORTROOT/bin/$XHOST-nm"
 
+#These can't be figured out by autoconf when cross-compiling
+export GLIB_CFLAGS=`$PKG_CONFIG --cflags glib-2.0 gthread-2.0`
+export GLIB_LIBS=`$PKG_CONFIG --libs glib-2.0 gthread-2.0`
+
+
 ./configure \
 	--host=$XHOST \
 	--enable-static \
