5c5657631a95d2a3b9db40fd6156477f2021ef6a
Brian Marshall <brian.marshall@colorado.edu>
2009-11-20 15:43:52 -0700

    Fix check test link errors

---
+++ check/data-manager/global.c
@@ -0,0 +1,17 @@
+
+// Copyright (c) 2008-2009, Regents of the University of Colorado.
+// This work was supported by NASA contracts NNJ05HE10G, NNC06CB40C, and
+// NNC07CB47C.
+
+#include "../../data-manager/server/bionet-data-manager.h"
+
+char * database_file = "bdm.db";
+bionet_bdm_t * this_bdm = NULL;
+
+// vim: ts=8 sw=4 sta
+// Emacs cruft
+// Local Variables:
+// mode: C
+// c-file-style: "Stroustrup"
+// indent-tabs-mode: nil
+// End:
