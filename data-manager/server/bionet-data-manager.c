1f4e04490f177200fbc812aa0f619d069a0d160f
Brian Marshall <brian.marshall@colorado.edu>
2010-04-07 14:52:28 -0600

    bdm usabililty enhancements:  - Fix bionet-data-manager help and option parsing  - Add more documentation to sample sync config  - Fix typeo

--- data-manager/server/bionet-data-manager.c
+++ data-manager/server/bionet-data-manager.c
@@ -81,9 +81,10 @@ void usage(void) {
 	"                                                certificates\n"
 	" -t,--tcp-sync-receiver [<port>]                Enable BDM synchonization over TCP. \n"
         "                                                Optionally specify the tcp port. Default: %d\n"
-#if ENABLE_ION
 	" -u,--no-resources                              Do not subscribe to any resources. Useful\n"
-	"                                                with --dtn-sync-receiver\n"
+	"                                                with --tcp-sync-receiver\n"
+#if ENABLE_ION
+	"                                                and  --dtn-sync-receiver\n"
 #endif
 	" -v,--version                                   Show the version number\n"
 	"\n"
@@ -227,8 +228,9 @@ int main(int argc, char *argv[]) {
 	switch (c) {
 
 	case '?':
+	case ':':
 	    usage();
-	    return 0;
+	    return 1;
 
 	case 'b':
 	    start_hab = 1;
@@ -240,9 +242,9 @@ int main(int argc, char *argv[]) {
 	    sync_sender_config_t * sync_config = NULL;
 	    sync_config = read_config_file(optarg);
 	    if (NULL == sync_config) {
-		g_log(BDM_LOG_DOMAIN, G_LOG_LEVEL_WARNING,
+		g_log(BDM_LOG_DOMAIN, G_LOG_LEVEL_ERROR,
 		      "Failed to parse config file %s", optarg);
-		continue;
+                return 1;
 	    }
 	    sync_config->last_entry_end_seq = -1;
 	    sync_config->last_entry_end_seq_metadata = -1;
@@ -250,9 +252,9 @@ int main(int argc, char *argv[]) {
 #if ENABLE_ION
                 if (bp_attach() < 0)
                 {
-                    g_log(BDM_LOG_DOMAIN, G_LOG_LEVEL_WARNING,
-                        "Can't attach to BP.");
-                    continue;
+                    g_log(BDM_LOG_DOMAIN, G_LOG_LEVEL_ERROR,
+                        "Can't attach to BP, but DTN syncing requested");
+                    return 1
                 }
                 bp_attached++;
 #else
@@ -407,9 +409,18 @@ int main(int argc, char *argv[]) {
 	    return 0;
 	    
 	default:
+            printf("Unknown option\n");
+	    usage();
+            return 1;
 	    break;
 	}
     } //while(1)
+
+    if(optind < argc){
+        printf("Extra options\n");
+        usage();
+        return 1;
+    }
 
 
     this_bdm = bionet_bdm_new(bdm_id);
