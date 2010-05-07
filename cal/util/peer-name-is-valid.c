53538fd29fec95941a6aad532b8c1ee92c68f6b7
Sebastian Kuzminsky <seb@highlab.com>
2010-05-07 11:59:05 -0600

    Don't make the program exit on CAL Event errors
    Before this commit, CAL would log errors in its Event sanity-checking
    code with the "ERROR" log level, which also causes the program to exit...
    
    This commit changes the log levels of those messages to just "WARNING",
    which doesnt make it exit.

--- cal/util/peer-name-is-valid.c
+++ cal/util/peer-name-is-valid.c
@@ -18,22 +18,22 @@ int cal_peer_name_is_valid(const char *peer_name) {
     int i;
 
     if (peer_name == NULL) {
-        g_log(CAL_LOG_DOMAIN, G_LOG_LEVEL_ERROR, "cal_peer_name_is_valid(): NULL peer_name passed in");
+        g_log(CAL_LOG_DOMAIN, G_LOG_LEVEL_WARNING, "cal_peer_name_is_valid(): NULL peer_name passed in");
         return 0;
     }
 
     for (i = 0; i < CAL_PEER_NAME_MAX_LENGTH; i ++) {
         if (peer_name[i] == (char)0) break;
         if (isprint(peer_name[i])) continue;
-        g_log(CAL_LOG_DOMAIN, G_LOG_LEVEL_ERROR, "cal_peer_name_is_valid(): byte at offset %d of peer_name \"%s\" is invalid", i, peer_name);
+        g_log(CAL_LOG_DOMAIN, G_LOG_LEVEL_WARNING, "cal_peer_name_is_valid(): byte at offset %d of peer_name \"%s\" is invalid", i, peer_name);
         return 0;
     }
     if (i == 0) {
-        g_log(CAL_LOG_DOMAIN, G_LOG_LEVEL_ERROR, "cal_peer_name_is_valid(): empty peer_name passed in");
+        g_log(CAL_LOG_DOMAIN, G_LOG_LEVEL_WARNING, "cal_peer_name_is_valid(): empty peer_name passed in");
         return 0;
     }
     if (i == CAL_PEER_NAME_MAX_LENGTH) {
-        g_log(CAL_LOG_DOMAIN, G_LOG_LEVEL_ERROR, "cal_peer_name_is_valid(): passed-in peer_name is too long (max %d)", CAL_PEER_NAME_MAX_LENGTH);
+        g_log(CAL_LOG_DOMAIN, G_LOG_LEVEL_WARNING, "cal_peer_name_is_valid(): passed-in peer_name is too long (max %d)", CAL_PEER_NAME_MAX_LENGTH);
         return 0;
     }
 
--- cal/util/topic-is-valid.c
+++ cal/util/topic-is-valid.c
@@ -18,22 +18,22 @@ int cal_topic_is_valid(const char *topic) {
     int i;
 
     if (topic == NULL) {
-        g_log(CAL_LOG_DOMAIN, G_LOG_LEVEL_ERROR, "cal_topic_is_valid(): NULL topic passed in");
+        g_log(CAL_LOG_DOMAIN, G_LOG_LEVEL_WARNING, "cal_topic_is_valid(): NULL topic passed in");
         return 0;
     }
 
     for (i = 0; i < CAL_TOPIC_MAX_LENGTH; i ++) {
         if (topic[i] == (char)0) break;
         if (isprint(topic[i])) continue;
-        g_log(CAL_LOG_DOMAIN, G_LOG_LEVEL_ERROR, "cal_topic_is_valid(): byte at offset %d of topic \"%s\" is invalid", i, topic);
+        g_log(CAL_LOG_DOMAIN, G_LOG_LEVEL_WARNING, "cal_topic_is_valid(): byte at offset %d of topic \"%s\" is invalid", i, topic);
         return 0;
     }
     if (i == 0) {
-        g_log(CAL_LOG_DOMAIN, G_LOG_LEVEL_ERROR, "cal_topic_is_valid(): empty topic passed in");
+        g_log(CAL_LOG_DOMAIN, G_LOG_LEVEL_WARNING, "cal_topic_is_valid(): empty topic passed in");
         return 0;
     }
     if (i == CAL_TOPIC_MAX_LENGTH) {
-        g_log(CAL_LOG_DOMAIN, G_LOG_LEVEL_ERROR, "cal_topic_is_valid(): passed-in topic is too long (max %d)", CAL_TOPIC_MAX_LENGTH);
+        g_log(CAL_LOG_DOMAIN, G_LOG_LEVEL_WARNING, "cal_topic_is_valid(): passed-in topic is too long (max %d)", CAL_TOPIC_MAX_LENGTH);
         return 0;
     }
 
