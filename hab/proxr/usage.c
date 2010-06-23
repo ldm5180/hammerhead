6e62f93bff711c08b144104be072783aafa8d575
Bo Pearce <bo.pearce@colorado.edu>
2010-06-23 10:55:08 -0600

    Edited usage.c to display help.

--- hab/proxr/usage.c
+++ hab/proxr/usage.c
@@ -6,5 +6,17 @@
 //
 void usage(void)
 {
-    printf("fill this in\n");
+    fprintf(stderr, 
+            "\nsim-hab must be ran with two options. -p and -a both followed\n"
+            "by a file location. To determine the file locations run the\n"
+            "findPort program.\n"
+            "\n"
+            "usage: sim-hab [OPTIONS]\n"
+            " -?, --help             Show this information.\n"
+            " -v, --version          Show the version number.\n"
+            " -i, --id <ID>          Use ID as the HAB-ID (defaults to\n"
+            "                        hostname if omitted).\n"
+            " -p --proxr <FILE>      Give the ProXR file location to open.\n"
+            " -a --arduino <FILE>    Give the Arduino file location to open.\n"
+            );
 }
