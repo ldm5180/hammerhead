d37edab3735da527afadca834c71157c51ba742e
Marek Sotola <marek.sotola@colorado.edu>
2010-06-29 11:53:04 -0600

    updating the header file with the new bip_net_connect_header.

--- cal/mdnssd-bip/client/cal-client-mdnssd-bip.h
+++ cal/mdnssd-bip/client/cal-client-mdnssd-bip.h
@@ -77,6 +77,24 @@ void *cal_client_mdnssd_bip_function(void *arg);
 
 extern void cal_client_mdnssd_bip_thread_destroy(cal_client_mdnssd_bip_t* thread_data);
 
+/**
+ * @brief Start to connect a bip net.
+ *
+ * @param peer_name The name of the peer whose net to connect to.  Only
+ *     used for log messages.
+ *
+ * @param net The net to connect.
+ *
+ * @param saddr The sock addr info struct
+ *
+ * @return The socket that the connection is in progress 
+ *
+ * @return -1 on failure (in which case the caller should destroy the net).
+ */
+
+int bip_net_connect_nonblock(void * cal_handle, const char *peer_name, bip_peer_network_info_t *net, const struct sockaddr *saddr);
+
+
 //
 // this is a linked list, each payload is a (struct cal_client_mdnssd_bip_service_context *)
 // we add the first one when we start the mDNS-SD browse running, then we add one each time we start a resolve
