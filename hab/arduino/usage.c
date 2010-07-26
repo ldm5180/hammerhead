6b6a544be44a930942bfce48211329bdca6cd5cf
Bo Pearce <bo.pearce@colorado.edu>
2010-07-26 12:39:50 -0600

    Modify arduino hab help menu.

--- hab/arduino/usage.c
+++ hab/arduino/usage.c
@@ -7,16 +7,14 @@
 void usage(void)
 {
     fprintf(stderr, 
-            "\nsim-hab must be ran with two options. -p and -a both followed\n"
-            "by a file location. To determine the file locations run the\n"
-            "findPort program.\n"
+            "\ncgba-sim-arduino-hab must be ran with one option. -a followed\n"
+            "by a file location.\n"
             "\n"
-            "usage: sim-hab [OPTIONS]\n"
+            "usage: cgba-sim-arduino-hab [OPTIONS]\n"
             " -?, --help             Show this information.\n"
             " -v, --version          Show the version number.\n"
             " -i, --id <ID>          Use ID as the HAB-ID (defaults to\n"
             "                        hostname if omitted).\n"
-            " -p --proxr <FILE>      Give the ProXR file location to open.\n"
             " -a --arduino <FILE>    Give the Arduino file location to open.\n"
             );
 }
