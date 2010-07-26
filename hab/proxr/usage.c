dabdcf18ea145db821ce8b5d03603c25ee06c049
Bo Pearce <bo.pearce@colorado.edu>
2010-07-26 12:38:12 -0600

    Modify proxr hab help menu.

--- hab/proxr/usage.c
+++ hab/proxr/usage.c
@@ -7,16 +7,14 @@
 void usage(void)
 {
     fprintf(stderr, 
-            "\nsim-hab must be ran with two options. -p and -a both followed\n"
-            "by a file location. To determine the file locations run the\n"
-            "findPort program.\n"
+            "\ncgba-sim-proxr-hab must be ran with one option. -p followed\n"
+            "by a file location.\n"
             "\n"
-            "usage: sim-hab [OPTIONS]\n"
+            "usage: cgba-sim-proxr-hab [OPTIONS]\n"
             " -?, --help             Show this information.\n"
             " -v, --version          Show the version number.\n"
             " -i, --id <ID>          Use ID as the HAB-ID (defaults to\n"
             "                        hostname if omitted).\n"
             " -p --proxr <FILE>      Give the ProXR file location to open.\n"
-            " -a --arduino <FILE>    Give the Arduino file location to open.\n"
             );
 }
