5852b273a6e8b1e066026ebf5aef9f00e0f70f5a
Bo Pearce <bo.pearce@colorado.edu>
2010-08-04 11:10:15 -0600

    Change to double. Missed from previous commit.

--- hab/proxr/cb-set-resource.c
+++ hab/proxr/cb-set-resource.c
@@ -33,7 +33,7 @@ void cb_set_resource(bionet_resource_t *resource, bionet_value_t *value)
     	    set_potentiometer(id, (int)data); 
 	    // set resources datapoint to new value
             content = data*POT_CONVERSION; 
-            bionet_resource_set_float(resource, content, NULL);
+            bionet_resource_set_double(resource, content, NULL);
             hab_report_datapoints(node);
             return;
         }
