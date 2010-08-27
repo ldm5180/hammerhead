b695ab6fc3c6d9483a516b26c535e233b0041976
Lenny Maiorani <lenny@colorado.edu>
2010-08-27 14:54:34 -0600

    add 3 check unit tests for bionet_resource_set_str()

--- check/libutil/check-libutil-resource-tests.c
+++ check/libutil/check-libutil-resource-tests.c
@@ -1000,6 +1000,42 @@ START_TEST (test_libutil_resource_set_double_2) {
 } END_TEST /* test_libutil_resource_set_double_2 */
 
 
+START_TEST (test_libutil_resource_set_string_0) {
+    bionet_resource_t * resource;
+
+    resource = bionet_resource_new(NULL, 
+				   BIONET_RESOURCE_DATA_TYPE_STRING, 
+				   BIONET_RESOURCE_FLAVOR_PARAMETER, 
+				   "resource");
+    fail_if(resource == NULL, "failed to create a perfectly normal resource\n");
+
+    fail_if (bionet_resource_set_str(resource, "foo", NULL),
+	"Failed to set the resource when the timestamp is NULL");
+} END_TEST /* test_libutil_resource_set_string_0 */
+
+
+START_TEST (test_libutil_resource_set_string_1) {
+    bionet_resource_t * resource;
+    struct timeval tv;
+
+    resource = bionet_resource_new(NULL, 
+				   BIONET_RESOURCE_DATA_TYPE_STRING, 
+				   BIONET_RESOURCE_FLAVOR_PARAMETER, 
+				   "resource");
+    fail_if(resource == NULL, "failed to create a perfectly normal resource\n");
+
+    gettimeofday(&tv, NULL);
+    fail_if (bionet_resource_set_str(resource, "foo", &tv),
+	"Failed to set the resource when the timestamp is defined");
+} END_TEST /* test_libutil_resource_set_string_1 */
+
+
+START_TEST (test_libutil_resource_set_string_2) {
+    fail_unless (bionet_resource_set_str(NULL, "foo", NULL),
+		 "Failed to detect NULL resource passed in.");
+} END_TEST /* test_libutil_resource_set_string_2 */
+
+
 START_TEST (test_libutil_resource_set_str_0) {
     bionet_resource_t *resource;
     int r;
@@ -1164,6 +1200,11 @@ void libutil_resource_tests_suite(Suite *s) {
     tcase_add_test(tc, test_libutil_resource_set_double_0);
     tcase_add_test(tc, test_libutil_resource_set_double_1);
     tcase_add_test(tc, test_libutil_resource_set_double_2);
+
+    /* bionet_resource_set_string() */
+    tcase_add_test(tc, test_libutil_resource_set_string_0);
+    tcase_add_test(tc, test_libutil_resource_set_string_1);
+    tcase_add_test(tc, test_libutil_resource_set_string_2);
 
     /* bionet_resource_set_str() */
     tcase_add_test(tc, test_libutil_resource_set_str_0);
