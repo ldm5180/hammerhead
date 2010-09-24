d4a90d8d113f24214339080f70bdf4be92198420
Brian Marshall <brian.marshall@colorado.edu>
2010-09-24 13:57:49 -0600

    Add check_bps_send_check

--- bps/check/bps-api-tests.c
+++ bps/check/bps-api-tests.c
@@ -27,11 +27,41 @@ START_TEST (check_bps_socket) {
 }
 END_TEST
 
+START_TEST (check_bps_send) {
+    int r;
+    ssize_t bytes;
+
+    int fd = bps_socket(0,0,0);
+    fail_unless(fd >=0, "bps_socket returned error", strerror(errno));
+
+
+    struct bps_sockaddr srcaddr, dstaddr;
+    strncpy(srcaddr.uri, "ipn:1.1", BPS_EID_SIZE);
+    strncpy(dstaddr.uri, "ipn:1.2", BPS_EID_SIZE);
+
+    r = bps_bind(fd, &srcaddr, sizeof(struct bps_sockaddr));
+    fail_unless(0==r, "bps_bind() failed", strerror(errno));
+
+    bytes = bps_sendto(fd, "0123456789", 11, MSG_MORE,
+            &srcaddr, sizeof(struct bps_sockaddr));
+    fail_unless(bytes==11, "bps_sendto() returned %d", bytes);
+
+    bytes = bps_sendto(fd, "0123456789", 11, 0,
+            &srcaddr, sizeof(struct bps_sockaddr));
+    fail_unless(bytes==11, "bps_sendto() returned %d", bytes);
+
+
+    r = bps_close(fd);
+    fail_unless(0==r, "bps_close failed: %s", strerror(errno));
+
+}
+END_TEST
 
 void bsp_api_tests_init(Suite *s) {
     TCase * tc = tcase_create ("bps-api");
 
     tcase_add_test (tc, check_bps_socket);
+    tcase_add_test (tc, check_bps_send);
 
     suite_add_tcase(s, tc);
 
