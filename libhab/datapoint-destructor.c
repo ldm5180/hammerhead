3622c45d25d7ab15f42a96aa5160c1113fe0d35a
Brian Marshall <brian.marshall@colorado.edu>
2010-09-30 10:56:01 -0600

    Remove unneccessary debug messages

--- libhab/datapoint-destructor.c
+++ libhab/datapoint-destructor.c
@@ -30,9 +30,6 @@ void libhab_datapoint_destructor(bionet_datapoint_t * dp, void * hash_table) {
 	return;
     }
 
-    g_log(BIONET_LOG_DOMAIN, G_LOG_LEVEL_DEBUG,
-	  "libhab_datapoint_destructor: Removing key %s from recent datapoint hash", 
-	  bionet_resource_get_name(resource));
 } /* libhab_datapoint_destructor() */
 
 // Emacs cruft
