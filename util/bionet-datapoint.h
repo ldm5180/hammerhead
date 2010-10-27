ef37cefc1e7b15637d024aaef95fe751b6071e54
Lenny Maiorani <lenny@colorado.edu>
2010-10-27 14:43:25 -0600

    fixing curly quotes in our licensing notification which somehow got copied in from somewhere.

--- util/bionet-datapoint.h
+++ util/bionet-datapoint.h
@@ -11,7 +11,7 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
 // Lesser General Public License for more details. A copy of the GNU 
 // Lesser General Public License v 2.1 can be found in the file named 
-// “COPYING.LESSER”.  You should have received a copy of the GNU Lesser 
+// "COPYING.LESSER".  You should have received a copy of the GNU Lesser 
 // General Public License along with this library; if not, write to the 
 // Free Software Foundation, Inc., 
 // 51 Franklin Street, Fifth Floor, 
